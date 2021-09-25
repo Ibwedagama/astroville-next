5422a9200d72b51e79ac78b3626e890d12be12bd
Ibwedagama <wardana.wedagama@gmail.com>
2021-09-25 18:48:33 +0700

    chore(quote-block): improve quote block component

--- components/Layout/QuoteBlock/QuoteBlock.js
+++ components/Layout/QuoteBlock/QuoteBlock.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const QuoteBlock = () => {
+const QuoteBlock = ({quote, author}) => {
   return (
     <div
       className="
@@ -8,8 +8,8 @@ const QuoteBlock = () => {
       md:p-12 lg:p-20
       "
     >
-      <h2 className="font-display text-xl leading-loose mb-4 md:text-2xl">Sky is Blue & Rose is Red</h2>
-      <p className="text-brand-pink font-bold">- Astroville</p>
+      <h2 className="font-display text-xl leading-loose mb-4 md:text-2xl">{quote}</h2>
+      <p className="text-brand-pink font-bold">- {author}</p>
     </div>
   )
 }
